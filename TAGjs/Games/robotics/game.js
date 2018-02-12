56d09c028f0452e02f53621f4268876c02915e70
Harrison Fackrell <31286336+harrisonfackrell@users.noreply.github.com>
2018-02-12 10:55:12 -0700

    Robotics Adventure Update

--- TAGjs/Games/robotics/game.js
+++ TAGjs/Games/robotics/game.js
@@ -25,11 +25,13 @@ var World = {
   ),
   rooms: [
     new Room("D13.foyer",
-      "You're standing the D13 foyer.",
+      "You're standing the D13 foyer. This is the third building on the NUAMES \
+      campus.",
       {
         "in": ["D13.reidroom","walk in to Mr. Reid's room"],
         "east": ["D13.grijalvaroom","east to Mr. Grijalva's room"],
-        "west": ["D13.workshop","west to the machine shop"]
+        "west": ["D13.workshop","west to the machine shop"],
+        "out": ["D13.outside","out the door to the parking lot"]
       },
       "D13 Foyer"
     ),
@@ -50,12 +52,95 @@ var World = {
       "Mr Grijalva's Classroom"
     ),
     new Room("D13.workshop",
-      "This is the machine shop.",
+      "This is the machine shop. There's meme music playing from the back of \
+      the room.",
       {
         "out": ["D13.foyer","walk out the door to the foyer"],
         "in": ["D13.workshopin","go further in to the workshop"]
       },
       "Machine Shop"
+    ),
+    new Room("D13.workshopin",
+      "This is the interior of the machine shop. Coming here without safety \
+      glasses is decidedly unsafe.",
+      {
+        "out": ["D13.workshop","walk out of the taped-off glasses-only zone"]
+      },
+      "Machine Shop Interior"
+    ),
+    new Room("D13.outside",
+      "You're in the D13 parking lot. This building is famous for being \
+      incredibly far away from the rest of the NUAMES campus.",
+      {
+        "in": ["D13.foyer","walk in through the doors"],
+        "east": ["nuames.tfield","east to the trampled field"]
+      },
+      "D13 Parking Lot"
+    ),
+    new Room("nuames.tfield",
+      "The field you're standing in is used by most if not all NUAMES students \
+      to get between D13 and the other buildings. It's not very healthy for the \
+      grass, and a clear path is worn, but the sidewalk is just too slow.",
+      {
+        "west": ["D13.outside","go west to D13"],
+        "east": ["nuames.busstop","east to the bus stop"]
+      },
+      "Trampled Field"
+    ),
+    new Room("nuames.busstop",
+      "You're standing at the bus stop. unfortunately, you seem to have \
+      forgotten your bus pass, so you won't be using public transportation to \
+      go anywhere anytime soon. It's too bad you don't have your own car yet.",
+      {
+        "west": ["nuames.tfield","go west to the trampled field"],
+        "north": ["nuames.gfield","north to the gym field"]
+      },
+      "Bus Stop"
+    ),
+    new Room("nuames.gfield",
+      "The field you're standing in us frequently used for gym activities. \
+      Amusingly, the room that the 'Fit for Life' class meets in is labeled \
+      'Engineering'--needless to say, they don't actually do a lot of their \
+      exercising there, and they often come out here instead.",
+      {
+        "south": ["nuames.busstop","go south to the bus stop"],
+        "east": ["nuames.basketball","east to the basketball court"],
+        "north": ["D2.outside","north to D2"]
+      },
+      "Gym Field"
+    ),
+    new Room("D2.outside",
+      "This is D2, probably the least used of all the NUAMES buildings.",
+      {
+        "south": ["nuames.gfield","go south to the gym field"],
+        "north": ["D3.outside","north to D3"],
+        "in": ["Nowhere","in to the building"]
+      },
+      "D2 Sidewalk"
+    ),
+    new Room("D3.outside",
+      "This is D3, which houses the NUAMES office",
+      {
+        "south": ["D2.outside","go south to D2"]
+      },
+      "D3 Sidewalk"
+    ),
+    new Room("nuames.basketball",
+      "This basketball court is strangely circular--clearly, it wasn't meant \
+      for serious competitive play.",
+      {
+        "west": ["nuames.gfield","go west to the gym field"],
+        "north": ["nuames.seminary","north to the seminary building"]
+      },
+      "Basketball Court"
+    ),
+    new Room("nuames.seminary",
+      "You're standing outside of the seminary building, which may or may not be \
+      an official part of campus.",
+      {
+        "south": ["nuames.basketball","south to the basketball court"]
+      },
+      "Seminary Building"
       )
   ],
   entities: [
@@ -106,6 +191,46 @@ var World = {
         }
       },
       "note"
+    ),
+    new Entity("D13.memecomputer","D13.workshopin",
+      "a computer, playing meme songs on loop",
+      {
+        look: function() {
+          output("It's a computer, playing meme songs on loop");
+          outputCommentary("The build team really likes their meme music. If \
+          the robotics team is in here, meme songs are going. They once \
+          finished an entire 10-hour video; I think they have a meme problem.");
+        },
+        attack: function() {
+          output("You begin to attack the computer--after all, the build team \
+          needs help overcoming their meme addiction. However, just before you \
+          strike, you pause to consider the fact that Mr. Reid would be angry \
+          if you broke the computer. You ultimately decide not to smash it.");
+          outputCommentary("The build team really likes their meme music. If \
+          the robotics team is in here, meme songs are going. They once \
+          finished an entire 10-hour video; I think they have a meme problem.");
+        },
+        "turn off": function() {
+          output("You turn off the computer. A dedicated robotic arm reaches \
+          over and turns it on again.");
+        }
+      },
+      "computer"
+    ),
+    new Entity("D3.glasses","D3.outside",
+      "a pair of safety glasses. How convenient",
+      {
+        look: function() {
+          output("It's a sturdy pair of plastic safety glasses: perfect for \
+          saving your eyes from flying debris");
+        },
+        take: function() {
+          output("You take the glasses and put them on.");
+          this.parent.location = "Inventory",
+          this.parent.description = "a pair of safety glasses"
+        }
+      },
+      "glasses"
       )
   ],
   obstructions: [
@@ -113,15 +238,47 @@ var World = {
       "D13.workshop",
       {
         nothing: function() {
-          output(this.parent.exits.in[1]);
+          getPlayer().methods.nothing();
+        },
+        in: function() {
+          if (inventoryContains("D3.glasses")) {
+            this.parent.location = "Nowhere";
+          }
+          movePlayerByInput(getInput());
         }
       },
       {
         "in": ["D13.workshopin","You can't go in to the shop unless you have \
           <strong>safety glasses</strong>"]
       },
-      "in"
-      )
+      "nothing"
+    ),
+    new Obstruction("D2.lock",
+      "D2.outside",
+      {
+        attack: function() {
+          output("You do your best, but the lock won't budge.");
+        }
+      },
+      {
+        "in": ["Nowhere","there is a lock on the door, preventing you from \
+        going in"]
+      },
+      "lock"
+    ),
+    new Obstruction("D3.lock",
+      "D3.outside",
+      {
+        attack: function() {
+          output("You do your best, but the lock won't budge.");
+        }
+      },
+      {
+        "in": ["Nowhere","there is a lock on the door, preventing you from \
+        going in"]
+      },
+      "lock"
+    )
   ],
   interceptors: [],
   conversations: [
